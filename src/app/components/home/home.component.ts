4830e820f02314e159182c30d1196685793f9822
Daniel Muremwa Mburu <daniel.muremwa@cellulant.io>
2023-01-19 11:11:32 +0300

    fixes memory leak

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
 import { IPost } from "../../types/ipost";
 import { PhotosService } from "../../utils/services/photos.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
     posts: Array<IPost> = [];
+    postQuerySubscription: Subscription;
     postQuery: string;
 
     constructor(private currentRoute: ActivatedRoute, public photoService: PhotosService) {}
@@ -18,23 +20,31 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void {
         // listen to change in params
         this.currentRoute.queryParams.subscribe((data) => {
-            this.photoService.postQuery.next(data['post-query']);
-            this.fetchPosts();
+            const postQueryParam = data['post-query'];
+
+            // if the param is empty/undefined don't set it
+            // unless there's a value on postQuery
+            if (postQueryParam || (!postQueryParam && this.postQuery)) {
+                this.photoService.postQuery.next(postQueryParam);
+                this.fetchPosts();
+            }
         });
 
-        this.photoService.postQuery.subscribe((value) => {
+        this.postQuerySubscription = this.photoService.postQuery.subscribe((value) => {
             this.postQuery = value;
-        })
+        });
 
         this.fetchPosts();
+    }
+
+    ngOnDestroy() {
+        this.postQuerySubscription.unsubscribe();
     }
 
     fetchPosts () {
         this.photoService.fetchPosts(this.postQuery).subscribe(({ success, posts }) => {
             if (success) {
                 this.posts = posts
-            } else {
-                console.log("An error occurred")
             }
         });
     }
