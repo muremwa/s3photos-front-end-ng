0d8c8a1217099e0767447084813bfa65db37a3f5
Daniel Muremwa Mburu <daniel.muremwa@cellulant.io>
2023-01-20 16:03:16 +0300

    adds test for entries pipe

--- src/app/utils/pipes/object-entries.pipe.spec.ts
+++ src/app/utils/pipes/object-entries.pipe.spec.ts
@@ -1,8 +1,19 @@
-import { ObjectEntriesPipe } from './object-entries.pipe';
+import {ObjectEntriesPipe} from './object-entries.pipe';
 
 describe('ObjectEntriesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ObjectEntriesPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+        const pipe = new ObjectEntriesPipe();
+        expect(pipe).toBeTruthy();
+    });
+
+    it('should return a 3D array of entries', () => {
+        const pipe = new ObjectEntriesPipe();
+        expect(pipe.transform({
+            name: 'chris',
+            age: 20
+        })).toEqual([
+            ['name', 'chris'],
+            ['age', 20]
+        ]);
+    })
 });
