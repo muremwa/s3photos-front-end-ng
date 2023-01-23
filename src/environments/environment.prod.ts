8d4847e3e1a487664a286adcf8e6d91493a1ec7d
Daniel Muremwa Mburu <daniel.muremwa@cellulant.io>
2023-01-23 15:07:16 +0300

    adds some configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+    production: true,
+    fetchPostsUrl: '/photos/api/index/',
+    uploadPostUrl: '/photos/api/upload/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
     production: false,
-    baseUrl: '127.0.0.1:8000',
     fetchPostsUrl: '/photos/api/index/',
     uploadPostUrl: '/photos/api/upload/',
 };
